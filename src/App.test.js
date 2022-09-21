a82ede2ce00e15b42887ab919ad65bf951603fdc
Caleb <caleb.dockal@gmail.com>
2022-09-21 15:00:54 -0400

    changed testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import "@testing-library/jest-dom";
 import App from "./App";
+import { ContextProvider } from "./contexts/ContextProvider";
 
 describe("render App", () => {
-  render(<App />);
+  test("Button Rendering", () => {
+    render(
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    );
+    const div = screen.getByTestId("div");
+    expect(div).toBeInTheDocument();
+  });
 });
